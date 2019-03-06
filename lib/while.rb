27f6aa18d6c328c8ee4ec67b60d4feccf4da4e9b
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-06 13:13:57 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,8 @@
 	def using_while
-	levitation_force = 10
+levitation_force = 6
 	
-while levitation_force < 6
+while levitation_force < 10
   levitation_force += 1
-  puts "You have now eaten #{levitation_force} hot dog(s)."
+  puts "Wingardium Leviosa"
 end
-# => nil (return value)
-puts "You ate a total of #{levitation_force} hot dogs!"
 

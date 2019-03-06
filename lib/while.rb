508516cf8bc98cd4e2a05f3357dd738976f46ad9
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-06 13:01:51 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,13 @@
 def using_while
 	levitation_force = 6
 	
+while num_of_hotdogs_eaten < 7
+  num_of_hotdogs_eaten += 1
+  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
 
+# => nil (return value)
+ 
+puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
 end
 
 

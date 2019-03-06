5769cf747a5926bb775d665fa30abacf8748a528
Marco Lombardini <marco.lombardini@gmail.com>
2019-03-06 13:03:40 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
-	levitation_force = 6
+	levitation_force = 10
 	
-while levitation_force < 7
+while levitation_force < 6
   levitation_force += 1
   puts "You have now eaten #{levitation_force} hot dog(s)."
 end
